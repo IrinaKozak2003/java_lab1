47c2677cf6fb78050b2a2eebd005a7f5f8272c5d
Pomidorka <030503ira@gmail.com>
2022-02-18 17:19:19 +0300

    Initial commit

---
+++ src/com/company/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.company;
+
+class HelloWorld {
+ public static void main(String[] args) {
+ // Display "Hello World!"
+ System.out.println("Hello World!");
+ }
+}
+//TODO:Add a new method
